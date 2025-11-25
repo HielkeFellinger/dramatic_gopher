049386a280ad22967a1186b82f4e72d1fb25d99a
Hielke Fellinger <ikben@hielkefellinger.nl>
2025-11-25 22:27:02 +0100

    Small SQL ite testing; start of moving the credentials from the save file to db

--- app/models/campaign.go
+++ app/models/campaign.go
@@ -1,5 +1,13 @@
 package models
 
 type Campaign struct {
-	Id string `json:"id"`
+	Id   string `json:"id"`
+	Name string `json:"name"`
+}
+
+type CampaignAccess struct {
+	Id         string `json:"id"`
+	Role       string `json:"role"`
+	UserId     string `json:"user_id"`
+	CampaignId string `json:"campaign_id"`
 }
--- app/models/db.go
+++ app/models/db.go
@@ -5,9 +5,11 @@ import "database/sql"
 var DB *sql.DB
 
 var UserService *userService = &userService{}
+var CampaignService *campaignService = &campaignService{}
 
 type userService struct {
 }
+type campaignService struct{}
 
 func (us *userService) GetUserByUsername(username string) (User, error) {
 	user := User{}
@@ -22,4 +24,28 @@ func (us *userService) GetUserById(userId int64) (User, error) {
 	sqlGetUserById := `SELECT * FROM users WHERE id = ?`
 	result := DB.QueryRow(sqlGetUserById, userId)
 	return user, result.Scan(&user.Id, &user.Name, &user.DisplayName, &user.Password)
+}
+
+func (cs *campaignService) GetCampaignById(campaignId int64) (Campaign, error) {
+	campaign := Campaign{}
+	sqlGetCampaignById := `SELECT * FROM campaigns WHERE id = ?`
+
+	result := DB.QueryRow(sqlGetCampaignById, campaignId)
+	return campaign, result.Scan(&campaign.Id, &campaign.Name)
+}
+
+func (cs *campaignService) GetCampaignsByUserId(userId int64) ([]Campaign, error) {
+	campaigns := []Campaign{}
+	sqlGetCampaignsByUserId := `SELECT * FROM campaigns WHERE id IN ( SELECT DISTINCT campaign_id FROM campaign_access WHERE user_id = ? )`
+
+	result := DB.QueryRow(sqlGetCampaignsByUserId, userId)
+	return campaigns, result.Scan(&campaigns)
+}
+
+func (cs *campaignService) CreateCampaign(campaign Campaign) (Campaign, error) {
+	returnCampaign := Campaign{}
+	sqlCreateCampaign := `INSERT INTO campaigns (name) VALUES (?) RETURNING *`
+
+	result := DB.QueryRow(sqlCreateCampaign, campaign.Name)
+	return returnCampaign, result.Scan(&returnCampaign.Id, &returnCampaign.Name)
 }
